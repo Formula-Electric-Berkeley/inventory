7254107d5026c1802088db095e963bc0088b875e
Raadwan Masum <piraadwan@gmail.com>
2024-07-24 12:21:56 -0700

    configure deployment

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -6,15 +6,15 @@ const Landing = () => {
         <div className='w-full flex justify-center'>
             <div className='w-[1000px]'>
                 <div className='w-full flex justify-between my-10'>
-                    <div className='w-[275px] bg-gray-300 text-center box-shadow rounded-2xl py-5'>
+                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-fuchsia-600'>OUT OF STOCK</p>
                     </div>
-                    <div className='w-[275px] bg-gray-200 text-center box-shadow rounded-2xl py-5'>
+                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-rose-600'>NOT PUT BACK</p>
                     </div>
-                    <div className='w-[275px] bg-gray-100 text-center box-shadow rounded-2xl py-5'>
+                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-blue-600'>LOW STOCK</p>
                     </div>
