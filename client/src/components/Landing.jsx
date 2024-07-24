0208c10406db4398540c2f11e94c72ddfbe54bf0
Raadwan Masum <piraadwan@gmail.com>
2024-07-24 11:56:37 -0700

    add table with dummy data

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,23 +1,25 @@
 import React, { useRef } from 'react';
+import Table from "./Table";
 
 const Landing = () => {
     return (
         <div className='w-full flex justify-center'>
             <div className='w-[1000px]'>
                 <div className='w-full flex justify-between my-10'>
-                    <div className='w-[250px] bg-gray-300 text-center box-shadow rounded-2xl py-7'>
+                    <div className='w-[275px] bg-gray-300 text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-fuchsia-600'>OUT OF STOCK</p>
                     </div>
-                    <div className='w-[250px] bg-gray-200 text-center box-shadow rounded-2xl py-7'>
+                    <div className='w-[275px] bg-gray-200 text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-rose-600'>NOT PUT BACK</p>
                     </div>
-                    <div className='w-[250px] bg-gray-100 text-center box-shadow rounded-2xl py-7'>
+                    <div className='w-[275px] bg-gray-100 text-center box-shadow rounded-2xl py-5'>
                         <p className='text-3xl'>99 ITEMS</p>
                         <p className='text-base text-blue-600'>LOW STOCK</p>
                     </div>
                 </div>
+                <Table />
             </div>
         </div>
     );
