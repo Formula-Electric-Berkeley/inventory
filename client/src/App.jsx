af09ea35b4f5d351e63d7933ccadb320f5dbe2a9
Raadwan Masum <piraadwan@gmail.com>
2024-09-02 16:28:51 -0700

    dynamically create item page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import './index.css';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
+import Item from './components/Item';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes basename="/inventory">
-                <Route path="/" element={<Landing />}>
-                </Route>
+                <Route path="/" element={<Landing />} />
+                <Route path="/item/:itemId" element={<Item />} />
             </Routes>
         </BrowserRouter>
     )
