63bf6c3afaf1f2b14d08567b6e3cc97bf5be6098
Raadwan Masum <piraadwan@gmail.com>
2024-09-02 21:53:00 -0700

    get rid of cards

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -4,19 +4,9 @@ const Landing = () => {
     return (
         <div className='w-full flex justify-center'>
             <div className='w-[1000px]'>
-                <div className='w-full flex justify-between my-10'>
-                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
-                        <p className='text-3xl'>99 ITEMS</p>
-                        <p className='text-base text-fuchsia-600'>OUT OF STOCK</p>
-                    </div>
-                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
-                        <p className='text-3xl'>99 ITEMS</p>
-                        <p className='text-base text-rose-600'>NOT PUT BACK</p>
-                    </div>
-                    <div className='w-[275px] bg-white text-center box-shadow rounded-2xl py-5'>
-                        <p className='text-3xl'>99 ITEMS</p>
-                        <p className='text-base text-blue-600'>LOW STOCK</p>
-                    </div>
+                <div className='w-full text-center my-10'>
+                    <h1 className="text-4xl font-bold">Inventory Management System</h1>
+                    <h2 className="text-xl mt-2">Created by Formula Electric at Berkeley</h2>
                 </div>
                 <Table />
             </div>
