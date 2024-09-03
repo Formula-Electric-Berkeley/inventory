213a977a295d3e0a6684b6552191474080f28b21
Raadwan Masum <piraadwan@gmail.com>
2024-09-02 21:43:07 -0700

    style item page

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { faArrowLeft, faMicrochip } from '@fortawesome/free-solid-svg-icons'
 
 const Item = () => {
     const { itemId } = useParams();
@@ -19,7 +21,6 @@ const Item = () => {
                 }
 
                 const data = await response.json();
-                console.log(data.body[0]);
                 setItemData(data.body[0]);
             } catch (error) {
                 setError(error.message);
@@ -28,6 +29,10 @@ const Item = () => {
 
         fetchParts();
     }, [itemId]);
+
+    const handleArrowOnClick = () => {
+        window.location.href = '/'
+    }
 
     if (error === 'Not Found') {
         return (
@@ -40,7 +45,45 @@ const Item = () => {
         )
     } else {
         return (
-            <div className='w-full flex justify-center mt-20'>
+            <div className='flex justify-center'>
+                <span onClick={handleArrowOnClick}>
+                    <FontAwesomeIcon className="fixed top-4 left-4 size-10 cursor-pointer" icon={faArrowLeft} />
+                </span>
+                <div className="text-center w-[1000px]">
+                    <h1 className="text-4xl font-semibold my-10">{itemData.description}</h1>
+                    <div className="flex justify-center items-center m-auto w-60 h-60 box-shadow rounded-2xl">
+                        <FontAwesomeIcon className="size-20" icon={faMicrochip} />
+                    </div>
+                    <div className="flex justify-between w-full mt-10">
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.box_id}</h1>
+                            <h1 className="text-xl">Box ID</h1>
+                        </div>
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.quantity}</h1>
+                            <h1 className="text-xl">Quantity</h1>
+                        </div>
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.mfg_part_number}</h1>
+                            <h1 className="text-xl">Manufacturer PN</h1>
+                        </div>
+                    </div>
+                    <div className="flex justify-between w-full mt-10">
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.digikey_part_number}</h1>
+                            <h1 className="text-xl">DigiKey PN</h1>
+                        </div>
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.mouser_part_number}</h1>
+                            <h1 className="text-xl">Mouser PN</h1>
+                        </div>
+                        <div className="w-64 py-5 box-shadow rounded-2xl">
+                            <h1 className="text-3xl font-bold">{itemData.jlcpcb_part_number}</h1>
+                            <h1 className="text-xl">JLCPCB PN</h1>
+                        </div>
+                    </div>
+                </div>
+                {/*
                 <div>
                     <h1>Box Id: {itemData.box_id}</h1>
                     <h1>Created By: {itemData.created_by}</h1>
@@ -53,6 +96,7 @@ const Item = () => {
                     <h1>Mouser Part Number: {itemData.mouser_part_number}</h1>
                     <h1>Quantity: {itemData.quantity}</h1>
                 </div>
+            */}
             </div>
         );
     }
