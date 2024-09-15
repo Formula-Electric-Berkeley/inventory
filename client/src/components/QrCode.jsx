f34264e02c782fe0108a9fec8d4557dbf6de4e5b
Raadwan Masum <piraadwan@gmail.com>
2024-09-15 13:36:14 -0700

    hide qr code overflow

--- client/src/components/QrCode.jsx
+++ client/src/components/QrCode.jsx
@@ -5,7 +5,7 @@ const ItemQrCode = ({ link, boxId, mfgNum, desc }) => {
     return (
         <>
         <div className="w-full">
-            <div id="printableDiv" className="w-[216px] h-[384px] border-2 border-black m-auto px-3 rounded-xl">
+            <div id="printableDiv" className="w-[216px] h-[384px] border-2 border-black m-auto px-3 rounded-xl overflow-hidden">
                 <QRCode className="mx-auto w-full" value={link} />
                 <p className="text-lg">Box Id: <span className="font-bold">{boxId}</span></p>
                 <p className="text-lg">Mfg PN: <span className="font-bold">{mfgNum}</span></p>
